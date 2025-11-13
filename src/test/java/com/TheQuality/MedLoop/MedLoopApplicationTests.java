2d39982cfdd1d17c783fc51f9f08f6f55a739846
ayoub mekni <ayoubmekni211@gmail.com>
2025-11-13 23:14:28 +0100

    first commit

---
+++ src/test/java/com/TheQuality/MedLoop/MedLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheQuality.MedLoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedLoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
